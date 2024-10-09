54e53c22b5b2756d4d1df48a2cc62c0340ab512b
1prcntethan <148378961+1prcntethan@users.noreply.github.com>
2024-10-08 21:52:58 -0700

    react navbar built

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,15 @@
-import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Tutorials from './pages/Tutorials';
+import { Link } from 'react-router-dom';
+import Navbar from "./components/navbar"
+import "./App.css"
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tutorials" element={<Tutorials />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
 
