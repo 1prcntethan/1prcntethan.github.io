0404682047f81dc8f8f299f854c8201ff660e19b
1prcntethan <148378961+1prcntethan@users.noreply.github.com>
2024-10-23 17:36:54 -0700

    FINISHED SKILL THING

--- my-app/src/pages/terminology.jsx
+++ my-app/src/pages/terminology.jsx
@@ -3,6 +3,8 @@ import "./terminology.css"
 import TermSectionTitle from "../components/terminology-section-title";
 import ShortFormList from "../components/shortformlist";
 import TerminologyList from "../components/terminologylist";
+import DifficultyToggle from "../components/difficultytoggle";
+
 
 export function Terminology() {
     return (
@@ -15,7 +17,22 @@ export function Terminology() {
             <ShortFormList />
             <TermSectionTitle title="terminology" />
             <TerminologyList />
-            
+            <TermSectionTitle title="skill difficulty chart" />
+
+            <div className="svg-gradient-container">
+                <svg className="svg-gradient" viewBox="0 0 2415 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="2415" height="80" rx="40" transform="matrix(1 0 0 -1 0 80)" fill="url(#paint0_linear_39_2)"/>
+                    <defs>
+                    <linearGradient id="paint0_linear_39_2" x1="0" y1="40" x2="2415" y2="40" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#B66DA3"/>
+                    <stop offset="1" stop-color="#BBCA88"/>
+                    </linearGradient>
+                    </defs>
+                </svg>
+            </div>
+
+            <DifficultyToggle />
+
         </>
     )
 }
