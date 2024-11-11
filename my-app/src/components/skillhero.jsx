5ab76673c581792ef04fd419439a5ad10ddd758c
1prcntethan <148378961+1prcntethan@users.noreply.github.com>
2024-11-10 23:33:20 -0800

    switch to map

--- my-app/src/components/skillhero.jsx
+++ my-app/src/components/skillhero.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./skillhero.css";
-import HeroSvg from './herosvg';
-import HeroBlurb from './heroblurb.jsx';
+import { heroBlurb } from '../utilites/heroblurb.js';
 import { heroSvg } from '../utilites/herosvg.js';
 
 
@@ -11,11 +10,13 @@ export default function SkillHero(skill) {
             <div className="skill-hero-title">
                 {skill.title}
             </div>
-            <HeroSvg identifier={skill.identifier}/>
-            {/* {heroSvg.get(skill.identifier)} */}
+            {/* <HeroSvg identifier={skill.identifier}/> */}
+            {heroSvg.get(skill.identifier)}
             
-            <HeroBlurb identifier={skill.identifier}/>
-            
+            {/* <HeroBlurb identifier={skill.identifier}/> */}
+            <div className="hero-blurb">
+                {heroBlurb.get(skill.identifier)}
+            </div>
         </div>
     )
 }
