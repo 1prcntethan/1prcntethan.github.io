4ecc942beae79ac2a23b32dc2ba2e49dcd9fb4b8
1prcntethan <148378961+1prcntethan@users.noreply.github.com>
2024-12-08 15:43:22 -0800

    fix some stuff, trying scroll snap

--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -25,6 +25,40 @@ export function Homepage() {
 
                 </div>
             </div>
+
+            <div className="container">
+                <div className="block-1-left">
+
+                </div>
+                <div className="block-1-text">
+                    <div className="block-1-text-title">What exactly is calisthenics?</div>
+                    <br />
+                    Calisthenics is a term used to describe a large variety of exercises, but the one thing they have in common is that all of them involve creating <strong><i>resistance through body weight</i></strong>. Whether you discovered calisthenics through seeing it at your local park, or saw a video on social media, it’s important you don’t rush the process. Despite how easy influencers make skills like the full planche look, in reality, it can take <strong><i>a couple years</i></strong> to accumulate the strength it takes to achieve that. Additionally, factors like height as weight can provide a significant handicap or advantage to learning certain skills. 
+                    <br />
+                    <br />
+                    If you are starting calisthenics from <strong>ZERO</strong>, expect to be doing basic exercises for at least <strong><i>6-12 months</i></strong>. Depending on your starting fitness level, you may be able to move on to skill progressions faster, but basics are the single most important thing to master when you want to achieve high difficulty skills. The journey to your dream skill may last many years, but by following the right path, <strong><i>no skill is impossible</i></strong>.
+                </div>
+                <div className="block-1-right">
+
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="block-1-left">
+
+                </div>
+                <div className="block-1-text">
+                    <div className="block-1-text-title">What exactly is calisthenics?</div>
+                    <br />
+                    Calisthenics is a term used to describe a large variety of exercises, but the one thing they have in common is that all of them involve creating <strong><i>resistance through body weight</i></strong>. Whether you discovered calisthenics through seeing it at your local park, or saw a video on social media, it’s important you don’t rush the process. Despite how easy influencers make skills like the full planche look, in reality, it can take <strong><i>a couple years</i></strong> to accumulate the strength it takes to achieve that. Additionally, factors like height as weight can provide a significant handicap or advantage to learning certain skills. 
+                    <br />
+                    <br />
+                    If you are starting calisthenics from <strong>ZERO</strong>, expect to be doing basic exercises for at least <strong><i>6-12 months</i></strong>. Depending on your starting fitness level, you may be able to move on to skill progressions faster, but basics are the single most important thing to master when you want to achieve high difficulty skills. The journey to your dream skill may last many years, but by following the right path, <strong><i>no skill is impossible</i></strong>.
+                </div>
+                <div className="block-1-right">
+
+                </div>
+            </div>
         </>
     )
 }
