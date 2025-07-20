571d34fab3c935f926a6dd433af5d1e5c579b989
Ethan T <148378961+1prcntethan@users.noreply.github.com>
2025-07-19 23:39:18 -0700

    PWA possibly working?

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import PWAPrompt from 'react-ios-pwa-prompt';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <>
+  <App/><PWAPrompt copyTitle="Install WINGS calisthenics app" />
+  </>
 );
