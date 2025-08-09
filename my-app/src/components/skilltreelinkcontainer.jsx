76a3a4f5457b5e5f4764af8e00e00193cc57244d
Ethan T <148378961+1prcntethan@users.noreply.github.com>
2025-08-09 13:57:29 -0700

    bug fix

--- my-app/src/components/skilltreelinkcontainer.jsx
+++ my-app/src/components/skilltreelinkcontainer.jsx
@@ -5,7 +5,7 @@ import SkillTreeLinks from "./skilltreelinks.jsx";
 export default function SkillTreeLinkContainer() {
   return (
     <>
-      <div className="tree-links-skill">
+      <div className="skilltree-links">
         <SkillTreeLinks link="/" text="home" />
       
         <SkillTreeLinks link="/tutorials" text="tutorials" />
