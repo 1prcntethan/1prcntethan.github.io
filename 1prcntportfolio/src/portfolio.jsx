a6c1711682da99742c8366abd630d5991477ee1b
1prcntethan <ethantay1prcnt@gmail.com>
2026-03-09 10:40:40 -0700

    adapt wheel spinning animation

--- 1prcntportfolio/src/portfolio.jsx
+++ 1prcntportfolio/src/portfolio.jsx
@@ -29,37 +29,202 @@ export default function Portfolio() {
         >
           <motion.div className="fixed-visual">
             <div class="visual-container">
-              <svg width="479" height="479" viewBox="0 0 479 479" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="239.5" cy="239.5" r="20" className="draw-circle"/>
-<circle cx="240" cy="239" r="82.5" className="draw-circle"/>
-<circle cx="240" cy="239" r="91.5" className="draw-circle"/>
+              <svg
+                width="479"
+                height="479"
+                viewBox="0 0 479 479"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="rotate-svg"
+              >
+                <circle
+                  cx="239.5"
+                  cy="239.5"
+                  r="20"
+                  className="draw-circle center1"
+                />
+                <circle
+                  cx="240"
+                  cy="239"
+                  r="82.5"
+                  className="draw-circle center1"
+                />
+                <circle
+                  cx="240"
+                  cy="239"
+                  r="91.5"
+                  className="draw-circle center1"
+                />
 
-<circle cx="239.5" cy="91.5" r="27" className="draw-circle"/>
-<circle cx="21.5" cy="21.5" r="20" transform="matrix(0 -1 -1 0 261 113)"   className="draw-circle"/>
-<line y1="-1.5" x2="149" y2="-1.5" transform="matrix(0 -1 -1 0 238 240)"   className="draw-line"/>
-<line y1="-1.5" x2="149" y2="-1.5" transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 238.295 239.002)"   className="draw-line"/>
-<line y1="-1.5" x2="149" y2="-1.5" transform="matrix(1 0 0 -1 238 238)"   className="draw-line"/>
-<line y1="-1.5" x2="149" y2="-1.5" transform="matrix(0.707107 0.707107 0.707107 -0.707107 239.709 237.588)"   className="draw-line"/>
-<line x1="239.5" y1="239" x2="239.5" y2="388" className="draw-line"/>
-<line x1="240.063" y1="239.356" x2="134.704" y2="344.714"   className="draw-line"/>
-<line x1="240" y1="239.5" x2="91" y2="239.5"   className="draw-line"/>
-<line x1="240.063" y1="240.063" x2="134.704" y2="134.704"   className="draw-line"/>
-<circle cx="344" cy="135" r="26.5" transform="rotate(90 344 135)"   className="draw-circle"/>
-<circle cx="21.5" cy="21.5" r="20" transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 344.361 165.463)"   className="draw-circle"/>
-<circle cx="386.5" cy="239.5" r="27" className="draw-circle"/>
-<circle cx="21.5" cy="21.5" r="20" transform="matrix(1 0 0 -1 365 261)"   className="draw-circle"/>
-<circle cx="344" cy="344" r="26.5"   className="draw-circle"/>
-<circle cx="21.5" cy="21.5" r="20" transform="matrix(0.707107 0.707107 0.707107 -0.707107 313.248 343.654)"   className="draw-circle"/>
-<circle cx="239.5" cy="387.5" r="27" className="draw-circle"/>
-<circle cx="239.5" cy="387.5" r="20" transform="rotate(90 239.5 387.5)"   className="draw-circle"/>
-<circle cx="135" cy="344" r="26.5"   className="draw-circle"/>
-<circle cx="135.057" cy="344.361" r="20" transform="rotate(135 135.057 344.361)"   className="draw-circle"/>
+                <line
+                  y1="-1.5"
+                  x2="149"
+                  y2="-1.5"
+                  transform="matrix(1 0 0 -1 238 238)"
+                  className="draw-line group1"
+                />
+                <circle
+                  cx="386.5"
+                  cy="239.5"
+                  r="27"
+                  className="draw-circle group1"
+                />
+                <circle
+                  cx="21.5"
+                  cy="21.5"
+                  r="20"
+                  transform="matrix(1 0 0 -1 365 261)"
+                  className="draw-circle group1"
+                />
 
-<circle cx="91.5" cy="239.5" r="27" className="draw-circle"/>
-<circle cx="91.5" cy="239.5" r="20" transform="rotate(180 91.5 239.5)"   className="draw-circle"/>
-<circle cx="135" cy="135" r="26.5"   className="draw-circle"/>
-<circle cx="135.057" cy="135.057" r="20" transform="rotate(-135 135.057 135.057)"   className="draw-circle"/>
-</svg>
+                <line
+                  y1="-1.5"
+                  x2="149"
+                  y2="-1.5"
+                  transform="matrix(0.707107 0.707107 0.707107 -0.707107 239.709 237.588)"
+                  className="draw-line group2"
+                />
+                <circle
+                  cx="344"
+                  cy="344"
+                  r="26.5"
+                  className="draw-circle group2"
+                />
+                <circle
+                  cx="21.5"
+                  cy="21.5"
+                  r="20"
+                  transform="matrix(0.707107 0.707107 0.707107 -0.707107 313.248 343.654)"
+                  className="draw-circle group2"
+                />
+
+                <line
+                  x1="239.5"
+                  y1="239"
+                  x2="239.5"
+                  y2="388"
+                  className="draw-line group3"
+                />
+                <circle
+                  cx="239.5"
+                  cy="387.5"
+                  r="27"
+                  className="draw-circle group3"
+                />
+                <circle
+                  cx="239.5"
+                  cy="387.5"
+                  r="20"
+                  transform="rotate(90 239.5 387.5)"
+                  className="draw-circle group3"
+                />
+
+                <line
+                  x1="240.063"
+                  y1="239.356"
+                  x2="134.704"
+                  y2="344.714"
+                  className="draw-line group4"
+                />
+                <circle
+                  cx="135"
+                  cy="344"
+                  r="26.5"
+                  className="draw-circle group4"
+                />
+                <circle
+                  cx="135.057"
+                  cy="344.361"
+                  r="20"
+                  transform="rotate(135 135.057 344.361)"
+                  className="draw-circle group4"
+                />
+
+                <line
+                  x1="240"
+                  y1="239.5"
+                  x2="91"
+                  y2="239.5"
+                  className="draw-line group5"
+                />
+                <circle
+                  cx="91.5"
+                  cy="239.5"
+                  r="27"
+                  className="draw-circle group5"
+                />
+                <circle
+                  cx="91.5"
+                  cy="239.5"
+                  r="20"
+                  transform="rotate(180 91.5 239.5)"
+                  className="draw-circle group5"
+                />
+
+                <line
+                  x1="240.063"
+                  y1="240.063"
+                  x2="134.704"
+                  y2="134.704"
+                  className="draw-line group6"
+                />
+                <circle
+                  cx="135"
+                  cy="135"
+                  r="26.5"
+                  className="draw-circle group6"
+                />
+                <circle
+                  cx="135.057"
+                  cy="135.057"
+                  r="20"
+                  transform="rotate(-135 135.057 135.057)"
+                  className="draw-circle group6"
+                />
+
+                <line
+                  y1="-1.5"
+                  x2="149"
+                  y2="-1.5"
+                  transform="matrix(0 -1 -1 0 238 240)"
+                  className="draw-line group7"
+                />
+                <circle
+                  cx="239.5"
+                  cy="91.5"
+                  r="27"
+                  className="draw-circle group7"
+                />
+                <circle
+                  cx="21.5"
+                  cy="21.5"
+                  r="20"
+                  transform="matrix(0 -1 -1 0 261 113)"
+                  className="draw-circle group7"
+                />
+
+                <line
+                  y1="-1.5"
+                  x2="149"
+                  y2="-1.5"
+                  transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 238.295 239.002)"
+                  className="draw-line group8"
+                />
+                <circle
+                  cx="344"
+                  cy="135"
+                  r="26.5"
+                  transform="rotate(90 344 135)"
+                  className="draw-circle group8"
+                />
+                <circle
+                  cx="21.5"
+                  cy="21.5"
+                  r="20"
+                  transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 344.361 165.463)"
+                  className="draw-circle group8"
+                />
+              </svg>
             </div>
           </motion.div>
           <motion.section className="intro-section">
@@ -79,8 +244,6 @@ export default function Portfolio() {
                 Thus, I strive for the ability to meld styles, functionality,
                 and aesthetic to the environment, blending intuitive design and
                 technical expertise. <br />
-                <br />
-                Reach for more.
               </motion.div>
             </div>
           </motion.section>
