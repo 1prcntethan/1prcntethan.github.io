5935ac7d483b7ed0982605ab47daf7afe03a3f37
Ethan T <ethantay1prcnt@gmail.com>
2026-09-11 23:10:56 -0700

    wings project page update

--- src/stories/wings-story.jsx
+++ src/stories/wings-story.jsx
@@ -71,8 +71,8 @@ export default function WingsStory({ onExit }) {
                 around and getting more comfortable with static webdev. And
                 while I was learning these basics, I wanted to think of an idea,
                 something I could build as a fun project to help me practice
-                what I learned. The first thing that came to mind was a website for
-                calisthenics, a sport I was (and still am) very passionate
+                what I learned. The first thing that came to mind was a website
+                for calisthenics, a sport I was (and still am) very passionate
                 about.
               </p>
               <p>
@@ -265,6 +265,90 @@ export default function WingsStory({ onExit }) {
                 alt="Early WINGS terminology page"
               />
             </div>
+          </div>
+          <div className="wings-content-center">
+            <p>
+              It's at this point, where the site started to look like a real
+              website, and it begins to come closer to the WINGS you see today.
+              The biggest change was the text. I had just been using a random
+              font I thought looked good, but I wanted something more unique,
+              and something that was minimal and aesthetic. The result was the
+              font you see today, "Major Mono Display", and "Fira Sans" for
+              accessible, readable body text. Looking back, they aren't the best
+              fit for each other, but they work well enough.
+            </p>
+          </div>
+          <div className="wings-content-twocol wings-image-left">
+            <div className="wings-image">
+              <img src="/wingstermnew.png" alt="Early WINGS terminology page" />
+            </div>
+            <div className="wings-image">
+              <img src="/wingsoldskillpage.png" alt="Early WINGS skill page" />
+            </div>
+          </div>
+          <div className="wings-content-twocol wings-image-right">
+            <div className="wings-text">
+              {/* <div className="wings-section-label">origins_</div> */}
+              <p>
+                Keep in mind that this was all done in static HTML/CSS. As you
+                can see, that tutorial page is for one skill. Currently, there
+                are around 50ish skills, and each one has it's own tutorial
+                page. If I had to do that all in static HTML/CSS, it would have
+                been a nightmare.
+              </p>
+              <p>
+                This is where I decided to move the website to React, allowing
+                for me to reuse components and allow the site to scale better. I
+                learned about passing in props to componets, and soon, with a
+                general outline, it took me just around 45 minutes to create a
+                single skill page. This was huge for me, and it allowed me to
+                focus more on writing the content on the site, and less on
+                having to code the same thing again and again.
+              </p>
+            </div>
+            <div className="wings-image">
+              <img src="/skillpagerepo.png" alt="WINGS repo w skill page jsx" />
+            </div>
+          </div>
+          <div className="wings-content-center">
+            <p>
+              During this time, I had been writing guides as well, for the
+              training section of the site. After those were added, and a decent
+              about of skills were uploaded to tutorials, I started the content
+              creation part of this project.
+            </p>
+          </div>
+          <div className="wings-content-twocol wings-image-left">
+            <div className="wings-text">
+              <p>
+                My editing is done completely on DaVinci Resolve, and honestly
+                the only reason I know how to do basic editing is because I used
+                to make Fortnite, Valorant, and Overwatch (went viral) montages,
+                back when I was in early high school. I started with just
+                random, general calisthenics reels, promoting my app at the end,
+                and I tried a few versions of app showcase. The app showcase
+                performed the best, so I kept making those types of reels. Each
+                reel performed better than the last, but I was getting maybe
+                1k-2k views on average.
+              </p>
+            </div>
+            <div className="wings-image">
+              <img src="/wingsdavinci.png" alt="Early WINGS skill page" />
+            </div>
+          </div>
+          <div className="wings-content-center">
+            <p>
+              Honestly, I don't really know if there really is a formula for
+              going viral on social media. But one day, one of the reels,
+              probably 2 weeks old, suddenly blew up. In a single day, it hit
+              1.5M, and over the course of the next 2 weeks, it leveled out at
+              around 2.5M views.
+            </p>
+            <p>
+              At the same time, the Instagram account gained around 15k
+              followers. And over the next 2 weeks, it rose to around 22k
+              followers. The pressure was on.
+            </p>
           </div>
         </motion.div>
       )}
